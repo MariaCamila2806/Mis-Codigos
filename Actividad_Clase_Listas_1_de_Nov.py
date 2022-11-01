78e6a5cd6bb6971d87e261b5b74a7bd81580c3f6
MariaCamila2806 <115585257+MariaCamila2806@users.noreply.github.com>
2022-11-01 13:48:53 -0500

    Create Actividad_Clase_Listas_1_de_Nov.py
    Actividad de listas en Python

---
+++ Actividad_Clase_Listas_1_de_Nov.py
@@ -0,0 +1,27 @@
+paises = ['Japón', 'Canada', 'Holanda','Mexico','Taiwan']
+print('Lista Original',paises)
+print()
+
+print(sorted(paises))
+print('Lista Original',paises)
+print()
+
+print(sorted(paises, reverse=True))
+print('Lista Original',paises)
+print()
+
+paises.reverse()
+print(paises)
+print()
+
+paises.reverse()
+print(paises)
+print()
+
+paises.sort()
+print(paises)
+print()
+
+paises.sort(reverse=True)
+print(paises)
+print()
