a286bf77c83eb04e5e78ca14b57bf1f462412315
MariaCamila2806 <115585257+MariaCamila2806@users.noreply.github.com>
2022-11-17 10:44:57 -0500

    Create Jornadas.java

---
+++ Jornadas.java
@@ -0,0 +1,42 @@
+public static void main(String[] args) {
+    int jor=Integer.parseInt(JOptionPane.showInputDialog("Digite jornada"));
+        if (jor == 1 )
+        {
+        System.out.println("Jornada "+jor);
+        System.out.println("Equipos 2 vs 1 --- Resultado 0:5 --- Ganador Equipo 1");
+        System.out.println("Equipos 4 vs 5 --- Resultado 3:2 --- Ganador Equipo 4");
+        System.out.println("Equipos 3 vs 6 --- Resultado 4:0 --- Ganador Equipo 3");
+        }
+
+        if (jor == 2 )
+        {
+            System.out.println("Jornada "+jor);
+            System.out.println("Equipos 2 vs 5 --- Resultado 1:4 --- Ganador Equipo 5");
+            System.out.println("Equipos 3 vs 4 --- Resultado 2:0 --- Ganador Equipo 3");
+            System.out.println("Equipos 6 vs 1 --- Resultado 0:4 --- Ganador Equipo 1");
+        }
+
+        if (jor == 3 )
+        {
+            System.out.println("Jornada "+jor);
+            System.out.println("Equipos 2 vs 3 --- Resultado 0:6 --- Ganador Equipo 3");
+            System.out.println("Equipos 1 vs 5 --- Resultado 2:2 --- Empate");
+            System.out.println("Equipos 6 vs 4 --- Resultado 1:1 --- Empate");
+        }
+
+        if (jor == 4 )
+        {
+            System.out.println("Jornada "+jor);
+            System.out.println("Equipos 5 vs 3 --- Resultado 1:4 --- Ganador Equipo 3");
+            System.out.println("Equipos 4 vs 1 --- Resultado 3:4 --- Ganador Equipo 1");
+            System.out.println("Equipos 6 vs 2 --- Resultado 3:1 --- Ganador Equipo 6");
+        }
+
+        if (jor == 5 )
+        {
+            System.out.println("Jornada "+jor);
+            System.out.println("Equipos 1 vs 3 --- Resultado 1:2 --- Ganador Equipo 3");
+            System.out.println("Equipos 5 vs 6 --- Resultado 2:1 --- Ganador Equipo 5");
+            System.out.println("Equipos 4 vs 2 --- Resultado 5:1 --- Ganador Equipo 4");
+        }
+    }
